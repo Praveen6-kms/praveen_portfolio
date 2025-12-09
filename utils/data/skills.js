25ccf6d1e84c02c2810427e8b36d14e47f64dbe0
Your Full Name <your.actual.email@example.com>
2025-12-09 06:25:23 +0530

    Portfolio updates

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,25 +1,16 @@
 export const skillsData = [
-  'HTML',
-  'CSS',
-  'Javascript',
-  'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
-  'Git',
-  'AWS',
-  'Bootstrap',
-  'Docker',
-  'Go',
-  'Figma',
-  'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
-]
+  "html",
+  "css",
+  "javascript",
+  "mongodb",
+  "bootstrap",
+  "python",
+  "git",
+  "mysql" // using mysql as SQL icon
+];
+
+
+
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
