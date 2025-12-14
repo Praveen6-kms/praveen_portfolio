90a82e5859ff8a0c96877c5616e554a01647b786
Your Full Name <your.actual.email@example.com>
2025-12-14 10:08:26 +0530

    Updated NTPC Simhadri project details

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -3,11 +3,11 @@ export const projectsData = [
     id: 1,
     name: "NTPC Simhadri Website",
     description:
-      "A responsive internal web portal for NTPC Simhadri with admin login, update posting, image upload, and secure backend integration.",
-    tools: ["Flask", "Bootstrap", "SQLite", "Python", "HTML", "CSS", "JavaScript"],
-    role: "Full Stack Developer",
+      "Developed a responsive internal web application for NTPC Simhadri to manage and display daily updates, announcements, and images. Implemented admin authentication, image upload functionality, and backend integration using Flask.",
+    tools: ["HTML", "CSS", "JavaScript", "Bootstrap", "Python", "Flask", "SQLite"],
+    role: "Web Development Intern",
     code: "https://github.com/Praveen6-kms/ntpc_website",
-    demo: "https://ntpc-website.onrender.com/#",
-    image: "/projects/ntpc.png"
-  }
+    demo: "https://ntpc-website.onrender.com/",
+    image: "/projects/ntpc.png",
+  },
 ];
